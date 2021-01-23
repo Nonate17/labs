21b8b004e721ca283bdec512d4d94dee314348d6
Nick Onate <nick@Nicks-MacBook-Air.local>
2021-01-22 21:24:51 -0700

    Lab 1 code

---
+++ src/labs/classlab1.java
@@ -0,0 +1,30 @@
+package labs;
+
+public class classlab1 {
+
+	public static void main(String[] args) {
+		int seats = 6;
+		System.out.println(seats);
+		double gcost = 81.94;
+		System.out.println(gcost);
+		char middlei = 'i';
+		System.out.println(middlei);
+		boolean hotOutSide = false;
+				System.out.println(hotOutSide);
+		String coustomerName = "Josh";
+		System.out.println(coustomerName);
+		String streetAddress = "2508 cimmaron dr";
+		System.out.println(streetAddress);
+		seats = seats - 2;
+		System.out.println(seats);
+		middlei = 'c';
+		System.out.println(middlei);
+		hotOutSide = true;
+		System.out.println(hotOutSide);
+		String fullname = coustomerName + " " + middlei + " Carly";
+		System.out.println(fullname);
+		System.out.println(" Hi, my name is " + fullname + " and i live at " + streetAddress + ".");
+
+	}
+
+}
